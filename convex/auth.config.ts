a5bb11d9bfc265254f31a3bb98f26bcf6850d1c8
Omar Gamal <o.gamal.abdulzaher@gmail.com>
2024-03-06 21:27:09 +0200

    initial setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://rare-humpback-48.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
